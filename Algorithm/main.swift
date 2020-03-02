8c7ace3980271dfe1b7f54dd14f5d9f37397646a
pirensen <pirensen@zhuanzhuan.com>
2020-03-02 11:50:10 +0800

    init repo

---
+++ Algorithm/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by pirensen on 2020/2/27.
+//  Copyright © 2020 pirensen. All rights reserved.
+//
+
+import Foundation
+
+
+
+
---
+++ Algorithm/贪心.swift
@@ -0,0 +1,13 @@
+//
+//  贪心.swift
+//  Algorithm
+//
+//  Created by pirensen on 2020/2/27.
+//  Copyright © 2020 pirensen. All rights reserved.
+//
+
+import Foundation
+
+func greddyAl() {
+    
+}
